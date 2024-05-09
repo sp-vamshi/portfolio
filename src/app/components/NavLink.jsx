6f2cb826e9b6eb9149660d4105a4003288ef69ad
sp-vamshi <spvamshi22@gmail.com>
2024-05-10 01:32:43 +0530

    added name to navlink componnet

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function ({ href, title }) {
+export default function NavLink({ href, title }) {
   return (
     <Link
       className="block py-2 pl-3 pr-4 text[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
